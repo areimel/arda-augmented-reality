6a184a910a9cb782f503b40c829cd52a21a42b60
Alec Reimel <alecreimel1@gmail.com>
2024-12-10 12:19:48 -0500

    QR code updates, cleanup, documentation

--- public/scripts/qrcode.js
+++ public/scripts/qrcode.js
@@ -3,8 +3,26 @@ function QRCodesSet(){
 	elements.forEach(element => {
 
 		const link = element.getAttribute("data-link");
-		const elID = element.getAttribute("id");
-		new QRCode(elID, link);
+		const size = element.getAttribute("data-size");
+
+		let pxSize = 250; //default size
+		if(size == "small"){
+			pxSize = 100;
+		} else if(size == "medium"){
+			pxSize = 250;
+		} else if(size == "large"){
+			pxSize = 500;
+		}
+		
+		//Create QR Code
+		var qrcode = new QRCode(element, {
+			text: link,
+			width: pxSize,
+			height: pxSize,
+			colorDark : "#000000",
+			colorLight : "#ffffff",
+			correctLevel : QRCode.CorrectLevel.H
+		});
 	});
 }
 
